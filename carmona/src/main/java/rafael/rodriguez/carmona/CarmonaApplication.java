e90bc7c35bd57bff9b41ae1f190a196657a60578
rrcarmonagfi <167966372+rrcarmonagfi@users.noreply.github.com>
2024-04-24 13:52:59 +0200

    proyecto mvn

---
+++ carmona/src/main/java/rafael/rodriguez/carmona/CarmonaApplication.java
@@ -0,0 +1,13 @@
+package rafael.rodriguez.carmona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmonaApplication.class, args);
+	}
+
+}
